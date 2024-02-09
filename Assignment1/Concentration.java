59d82af7abde2c7022e88080a0ce644cc48bd2ca
ZENITH-jpg <82682206+ZENITH-jpg@users.noreply.github.com>
2024-02-09 10:09:17 -0500

    fixed bugs

--- Assignment1/Concentration.java
+++ Assignment1/Concentration.java
@@ -2,7 +2,7 @@
  *Name: Tristan Cao
  *Date:
  *Teacher: Mr. Guglielmi
- *Description: The driver class for the project, main method runs the methods contained in the other files
+ *Description: 
 */
 import java.util.Scanner;
 public class Concentration{
@@ -26,32 +26,36 @@ public class Concentration{
    }
    void game(){
       display();
-      while(matches < 8 && misses < 10){
+      while(matches < 8 && misses < 10) {
          int x1, y1, x2, y2;
-         while(true){
+         do {
             System.out.println("Enter the column for first card (1-4).");
             x1 = input();
             System.out.println("Enter the row for first card (1-4).");
             y1 = input();
-            if(found[y1][x1]) 
+            if(found[y1][x1]) {
                System.out.println("Cannot pick a card that is already found.");
-            else
-               break;
-         }
-         while(true){
+               x1 = -1;
+            }
+         } while(x1 == -1);
+         do {
             System.out.println("Enter the column for second card (1-4).");
             x2 = input();
             System.out.println("Enter the row for second card (1-4).");
             y2 = input();
-            if(x1 == x2 && y1 ==y2)
+            if(x1 == x2 && y1 ==y2) {
                System.out.println("Cannot choose the same card for card 1 and 2.");
-            else if(found[y1][x1]) 
+               x2 = -1;
+            }
+            else if(found[y2][x2]) {
                System.out.println("Cannot pick a card that is already found.");
-            else
-               break;
-         }
-         if(bank[y1][x1] == bank[y2][x2]) matches++;
-         else misses++;
+               x2 = -1;
+            }
+         } while(x2 == -1);
+         if(bank[y1][x1] == bank[y2][x2]) 
+            matches++;
+         else 
+            misses++;
          found[y1][x1] = true;
          found[y2][x2] = true;
          display();
@@ -60,20 +64,22 @@ public class Concentration{
             found[y2][x2] = false;
          }
       }
-      if(matches = 8){
+      if(matches == 8){
          System.out.println("You win!");
       }else{
          System.out.println("You lose.");
       }
    }
    int input(){
-      while(true){
-         int in = s.nextInt()-1;
-         if(0>in || in>=4)
+      int in;
+      do {
+         in = s.nextInt()-1;
+         if(0>in || in>=4){
             System.out.println("Invalid input, must be a integer from 1-4.");
-         else 
-            return in;
-      }
+            in = -1;
+         }
+      } while(in == -1);
+      return in;
    }
    void display(){
       for(int i = 0; i < 4; i++){
