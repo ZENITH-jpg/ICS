ed0da6f3cef638ed839664b68e81cccd43beacda
ZENITH-jpg <82682206+ZENITH-jpg@users.noreply.github.com>
2024-02-09 09:41:43 -0500

    game

--- Assignment1/Concentration.java
+++ Assignment1/Concentration.java
@@ -26,20 +26,29 @@ public class Concentration{
    }
    void game(){
       display();
-      while(matches < 8 || misses < 10){
+      while(matches < 8 && misses < 10){
          int x1, y1, x2, y2;
-         System.out.println("Enter the column for first card (1-4)");
-         x1 = input();
-         System.out.println("Enter the row for first card (1-4)");
-         y1 = input();
          while(true){
-            System.out.println("Enter the column for second card (1-4)");
+            System.out.println("Enter the column for first card (1-4).");
+            x1 = input();
+            System.out.println("Enter the row for first card (1-4).");
+            y1 = input();
+            if(found[y1][x1]) 
+               System.out.println("Cannot pick a card that is already found.");
+            else
+               break;
+         }
+         while(true){
+            System.out.println("Enter the column for second card (1-4).");
             x2 = input();
-            System.out.println("Enter the row for second card (1-4)");
+            System.out.println("Enter the row for second card (1-4).");
             y2 = input();
             if(x1 == x2 && y1 ==y2)
-               System.out.println("Cannot choose the same card for card 1 and 2");
-            else break;
+               System.out.println("Cannot choose the same card for card 1 and 2.");
+            else if(found[y1][x1]) 
+               System.out.println("Cannot pick a card that is already found.");
+            else
+               break;
          }
          if(bank[y1][x1] == bank[y2][x2]) matches++;
          else misses++;
@@ -51,12 +60,17 @@ public class Concentration{
             found[y2][x2] = false;
          }
       }
+      if(matches = 8){
+         System.out.println("You win!");
+      }else{
+         System.out.println("You lose.");
+      }
    }
    int input(){
       while(true){
          int in = s.nextInt()-1;
          if(0>in || in>=4)
-            System.out.println("Invalid input, must be a integer from 1-4");
+            System.out.println("Invalid input, must be a integer from 1-4.");
          else 
             return in;
       }
