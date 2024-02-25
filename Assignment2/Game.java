7a87f602ecfb38aa9661b72dde119e90be45dda4
ZENITH-jpg <82682206+ZENITH-jpg@users.noreply.github.com>
2024-02-25 01:41:30 -0500

    rehull

--- Assignment2/Game.java
+++ Assignment2/Game.java
@@ -5,36 +5,38 @@
  *Description: 
 */
 import java.util.Scanner;
+import java.util.Random;
 public class Game {
-   private Monster p;
-   private Monster m;
+   private Player p;
+   private Zombie m;
    private Scanner s;
    private int turn; //odd for player even for monster
    private int monLeft;
+   private static Random r;
    
    public Game(){
-      p = new Monster(0);
-      m = new Monster(1);
+      p = new Player();
+      m = new Zombie();
       s = new Scanner(System.in);
+      r = new Random();
       turn = 1;
       monLeft = 9;
    }
    
    public void beginGame(){
-      Monster.createRandom();
       while(p.getHp() > 0 && m.getHp() > 0){
          System.out.printf("TURN %d: %s is at %d health and %s is at %d health\n", turn, p.getName(), p.getHp(), m.getName(), m.getHp());
          if(turn % 2 == 0) {
-            turn(m,p,m.chooseAttack());
+            turn(Game.chooseAttack());
          }
          else {
             System.out.println("Enter 1 to attack and 2 to defend!");
-            turn(p,m,input());
+            turn(input());
          }
          turn++;
          if(m.getHp() <= 0){
             System.out.println("A monster has been slain");
-            m = new Monster(1);
+            m = new Zombie();
          }
       }
       if(p.getHp() > 0)
@@ -43,14 +45,14 @@ public class Game {
          System.out.println("Player Loses");
    }
    
-   private void turn(Monster a, Monster b, int attack){
-      if(a.usedDef()){
+   private void turn(int attack){
+      /*if(a.usedDef()){
          a.undefend();
          a.flipDef();
       }
       if(attack == 1){
          System.out.printf("%s Attacks!\n", a.getName());
-         if(b.getDefence() < a.makeAttack()){
+         if(b.getDefence() < a.makeAttack(r)){
             b.setHp(b.getHp() - a.getDamage());
             System.out.printf("The attack suceeded! %s takes %d damage.\n",b.getName(), m.getDamage());
          }else{
@@ -60,8 +62,7 @@ public class Game {
          System.out.printf("%s Defends!\n", a.getName());
          a.defend();
          a.flipDef();
-      }
-      
+      }*/
    }
    
    private int input(){
@@ -74,5 +75,9 @@ public class Game {
          } 
       } while (in == -1) ;
       return in;
+   }
+   
+   private static int chooseAttack(){
+      return r.nextInt(2)+1;
    }
 }
---
+++ Assignment2/Zombie.java
@@ -0,0 +1,49 @@
+/*
+ *Name: Tristan Cao
+ *Date: 02-24-2024
+ *Teacher: Mr. Guglielmi
+ *Description: 
+*/
+import java.util.Scanner;
+import java.io.File;
+import java.util.Random;
+public class Zombie{
+   private int atk;
+   private int def;
+   private int hp;
+   private int dmg;
+   private String name;
+   
+   public int makeAttack(Random r){
+      return this.atk + r.nextInt(20)+1;
+   }
+   public int getAttack(){
+      return this.atk;
+   }
+   public int getDefence(){
+      return this.def;
+   }
+   public int getHp(){
+      return this.hp;
+   }
+   public int getDamage(){
+      return this.dmg;
+   }
+   public String getName(){
+      return this.name;
+   }
+
+   public Zombie(){
+      try{
+          Scanner s = new Scanner(new File("Zombie.txt"));
+          this.atk = s.nextInt();
+          this.def = s.nextInt();
+          this.hp = s.nextInt();
+          this.dmg = s.nextInt();
+          this.name = "Zombie";
+          s.close();
+      }catch(Exception e){
+
+      }
+   }
+}
