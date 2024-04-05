ff731c6efeaab2000243bafcfdccd5a4ac033598
ZENITH-jpg <82682206+ZENITH-jpg@users.noreply.github.com>
2024-04-05 10:17:57 -0400

    update

--- CaoT_Assignment5/Main.java
+++ CaoT_Assignment5/Main.java
@@ -1,17 +1,44 @@
+/* Author: Tristan Cao
+Date: 30/03/2024
+Description: Main class to test code and sorting algorithms
+*/
 import java.util.Scanner;
 public class Main{
    static Scanner s;
    static Roster r;
    public static void main(String[] args){
-      s = new Scanner(System.in);
+      s = new Scanner(System.in); 
       System.out.println("Input file to read players from");
-      r = new Roster(s.nextLine());
+      r = new Roster(s.nextLine()); // get input
       System.out.println("Roster before changes: ");
-      r.display();
+      r.display(); // display roster before sorting
       System.out.println("Type 1 if you want to sort roster by goals and any other key if you want to sort by points");
-      if(s.nextLine().equals("1")) r.sort(1);
+      if(s.nextLine().equals("1")) r.sort(1); // sort roster by goals or points
       else r.sort(2);
       System.out.println("Roster after changes: ");
-      r.display();
+      r.display(); // display sorted roster
+      boolean b = true;
+      while(b){
+         System.out.println("Type 1 to search for a player by name, 2 to search for a player by goals, and any other key to search by assists");
+         String a = s.nextLine();
+         int i = -1;
+         if(a.equals("1")){
+            System.out.println("Input player name");
+            i = r.getPlayer(s.nextLine());
+         }else if(a.equals("2")){
+            System.out.println("Input player goals");
+            i = r.getPlayerByGoals(Integer.parseInt(s.nextLine()));
+         }else{
+            System.out.println("Input player assists");
+            i = r.getPlayerByAssists(Integer.parseInt(s.nextLine()));
+         }
+         if(i != -1){
+            System.out.println(r.players[i].toString());
+         }else{
+            System.out.println("Player not found!");
+         }
+         System.out.println("Type 1 to break out the program, any other key to continue");
+         if(s.nextLine().equals("1")) b = false;
+      }
    }
 }
--- CaoT_Assignment5/Roster.java
+++ CaoT_Assignment5/Roster.java
@@ -49,42 +49,47 @@ public class Roster{
 		}
 		return;
 	}
-	public void sort(int x){
+	//insert assignment methods below this point
+   public void sort(int x){ // public sorting method, 1 = sort by goal else sort by points
       mergeSort(players,18,x);
    }
-	//insert assignment methods below this point
-   private void mergeSort (HockeyPlayer[] arr, int n, int method){
-      if(n == 1) return;
-      int x = n/2;
-      HockeyPlayer[] left = new HockeyPlayer[x];
-      HockeyPlayer[] right = new HockeyPlayer[n-x];
-      for(int i = 0; i < x; i++){
+   private static void mergeSort (HockeyPlayer[] arr, int n, int method){ // takes in array to sort, length of array, and sort by points or goals
+      if(n == 1) return; // if array is size of 1, return the array to be merged
+      int x = n/2; // split array in 2
+      HockeyPlayer[] left = new HockeyPlayer[x]; // array for left side of split
+      HockeyPlayer[] right = new HockeyPlayer[n-x]; // right side
+      for(int i = 0; i < x; i++){ // fill arrays
          left[i] = arr[i];
       }
       for(int i = x; i < n; i++){
          right[i-x] = arr[i];
       }
-      mergeSort(left, x, method);
+      mergeSort(left, x, method); // break down further
       mergeSort(right, n-x, method);
-      if(method == 1){
-         mergeGoals(arr, right, left, x, n-x);
-      }else{
-         mergePoints(arr, right, left, x, n-x);
-      }
+      merge(arr, right, left, x, n-x, method); // merge
+
    }
-	private void mergeGoals (HockeyPlayer a[], HockeyPlayer r[], HockeyPlayer l[], int left, int right){
-		int i = 0, j = 0, k = 0;
+	private static void merge (HockeyPlayer a[], HockeyPlayer r[], HockeyPlayer l[], int left, int right, int method){  //merging method
+		int i = 0, j = 0, k = 0;//indexes for right, left, merged arr
 		
-		while (i < left && j < right){
-			if (l[i].getGoals() <= r[j].getGoals()){
+		while (i < left && j < right){ // while index for right and left are less than their arr size
+         boolean b = false;
+         if(method == 1){ // compare is left criteria is less than right
+            b = l[i].getGoals() <= r[j].getGoals();
+         }else if(method == 2){
+            b = l[i].getPoints() <= r[j].getPoints();
+         }else{
+            b = l[i].getAssists() <= r[j].getAssists();
+         }
+			if (b){ // if left array element at index has less, put in array first
 				a[k++] = l[i++];
 			}
 			else {
-				a[k++] = r[j++];
+				a[k++] = r[j++]; // else right is less so put that in first
 			}
 		}
 		
-		while (i < left){
+		while (i < left){ // when one array is completly added into the merged array, add the remaining of the other
 			a[k++] = l[i++];
 		}
 		
@@ -93,25 +98,45 @@ public class Roster{
 		}
 	
 	}
-   private void mergePoints (HockeyPlayer a[], HockeyPlayer r[], HockeyPlayer l[], int left, int right){
-		int i = 0, j = 0, k = 0;
-		
-		while (i < left && j < right){
-			if (l[i].getPoints() <= r[j].getPoints()){
-				a[k++] = l[i++];
-			}
-			else {
-				a[k++] = r[j++];
-			}
-		}
-		
-		while (i < left){
-			a[k++] = l[i++];
-		}
-		
-		while (j < right){
-			a[k++] = r[j++];
-		}
-	
-	}
+   //searching methods for finding players in roster
+   public int getPlayer(String s){
+      for(int i = 0; i < players.length; i++){
+         if(players[i].getName().equals(s)){
+            return i;
+         }
+      }
+      return -1;
+   }
+   public int getPlayerByGoals(int x){
+      sort(1);
+      int l = 0;
+      int h = players.length-1;
+      while(l<=h){
+         int mid = (l+h)/2;
+         if(players[mid].getGoals() == x){
+            return mid;
+         }else if(players[mid].getGoals() > x){
+            h = mid-1;
+         }else{
+            l = mid+1;
+         }
+      }
+      return -1;
+   }
+   public int getPlayerByAssists(int x){
+      sort(3);
+      int l = 0;
+      int h = players.length-1;
+      while(l<=h){
+         int mid = (l+h)/2;
+         if(players[mid].getAssists() == x){
+            return mid;
+         }else if(players[mid].getAssists() > x){
+            h = mid-1;
+         }else{
+            l = mid+1;
+         }
+      }
+      return -1;
+   }
 }
