6195edcdf093a6f3b891ee121cd49f69d7010f36
ZENITH-jpg <82682206+ZENITH-jpg@users.noreply.github.com>
2024-04-25 10:20:57 -0400

    added assignment 7

---
+++ CaoT_Assignment7/Term.java
@@ -0,0 +1,40 @@
+public class Term{
+	
+	protected int coeff;
+	protected int exp;
+	
+	Term (int c, int e){
+		coeff = c;
+		exp = e;
+	}
+	
+	public boolean isGreater (Term t){
+		
+		if (this.exp > t.getExp()){ // changed to check exp first
+			return true;
+		}
+		else if (this.exp == t.getExp() && this.coeff > t.getCoeff()){ // changed to check if exp are equal
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public int evaluate (int x){
+	    return (int) ( Math.pow (x * this.coeff, this.exp));
+	}
+	
+	public String toString(){
+		return this.coeff + "x^" + this.exp;
+	}
+	
+	public int getCoeff(){
+		return this.coeff;
+	}
+	
+	public int getExp(){
+		return this.exp;
+	}
+}
+
